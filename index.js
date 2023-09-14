45482c6e207c1fa8ed9c728bd4f6736276ed280a
Garrett Hale <Garrett.hale@yahoo.com>
2023-09-14 16:06:16 -0500

    portfolio first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+function linkedIn() {
+  window.location.href = 'https://www.linkedin.com/in/garrett-hale-73a0a384/';
+}
+
+function scrollToProjects() {
+  document.getElementById('projects').scrollIntoView();
+}
