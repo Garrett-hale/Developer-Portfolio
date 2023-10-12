a991ff6851f5a19350dfaef0db25e285d87b2c27
Garrett <Garrett.hale@yahoo.com>
2023-10-12 14:02:49 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,44 +2,32 @@ var navCountDown = 500;
 var mouseOverNav = false;
 var mouseOverDropDown = false;
 
-function linkedIn() {
-  window.location.href = 'https://www.linkedin.com/in/garrett-hale-73a0a384/';
+function websiteLink(link) {
+  window.location.href = link;
 }
 
 function gitHub() {
   window.location.href = 'https://github.com/Garrett-hale';
 }
 
-function scrollToSkills() {
-  document.getElementById('skills').scrollIntoView();
+function scrollToView(scrollToId) {
+  document.getElementById(scrollToId).scrollIntoView();
 }
 
-function scrollToProjects() {
-  document.getElementById('projects').scrollIntoView();
+function navShow(trueOrFalse) {
+  if (trueOrFalse == true) {
+    document.getElementById("dropdown-content").style.visibility = "visible";
+  } else if (trueOrFalse == false) {
+    document.getElementById("dropdown-content").style.visibility = "hidden";
+  }
 }
 
-function scrollToEducation() {
-  document.getElementById('education').scrollIntoView();
-}
-
-function scrollToContacts() {
-  document.getElementById('contacts').scrollIntoView();
-}
-
-function navShow() {
-  document.getElementById("dropdown-content").style.visibility = "visible";
-}
-
-function navHide() {
+function navHideTimer() {
   var timer = setTimeout(function() {
     if (mouseOverNav == false && mouseOverDropDown == false) {
       document.getElementById("dropdown-content").style.visibility = "hidden";
     }
   }, navCountDown);
-}
-
-function navClickHide() {
-  document.getElementById("dropdown-content").style.visibility = "hidden";
 }
 
 function mouseNav(trueOrFalse) {
@@ -48,4 +36,16 @@ function mouseNav(trueOrFalse) {
 
 function mouseDropDown(trueOrFalse) {
   mouseOverDropDown = trueOrFalse;
+}
+
+//Calculator app
+
+function createElementAndAppend(elementTag, elementText, parentId) {
+  // Create element:
+  const element = document.createElement(elementTag);
+  element.innerText = elementText;
+
+  // Append to body:
+  //document.#header.appendChild(element);
+  document.getElementById(parentId).appendChild(element);
 }
