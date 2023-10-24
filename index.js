35b40ef80dbcacc3a4e4383a5ecaff7ce972ca70
Garrett Hale <Garrett.hale@yahoo.com>
2023-10-24 15:34:46 -0500

    update

--- index.js
+++ index.js
@@ -1,7 +1,35 @@
-function linkedIn() {
-  window.location.href = 'https://www.linkedin.com/in/garrett-hale-73a0a384/';
+var navCountDown = 500;
+var mouseOverNav = false;
+var mouseOverDropDown = false;
+
+function websiteLink(link) {
+  window.location.href = link;
 }
 
-function scrollToProjects() {
-  document.getElementById('projects').scrollIntoView();
+function scrollToView(scrollToId) {
+  document.getElementById(scrollToId).scrollIntoView();
+}
+
+function navShow(trueOrFalse) {
+  if (trueOrFalse == true) {
+    document.getElementById("dropdown-content").style.visibility = "visible";
+  } else {
+    document.getElementById("dropdown-content").style.visibility = "hidden";
+  }
+}
+
+function navHideTimer() {
+  var timer = setTimeout(function() {
+    if (mouseOverNav == false && mouseOverDropDown == false) {
+      document.getElementById("dropdown-content").style.visibility = "hidden";
+    }
+  }, navCountDown);
+}
+
+function mouseNav(trueOrFalse) {
+  mouseOverNav = trueOrFalse;
+}
+
+function mouseDropDown(trueOrFalse) {
+  mouseOverDropDown = trueOrFalse;
 }
